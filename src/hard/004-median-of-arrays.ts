57fcf087d8e49856896374ce5b34a6091596befa
Timo Traulsen <Timo.Traulsen@deutschebahn.com>
2022-08-27 15:53:28 +0200

    tried to speed up merging arrays

--- src/hard/004-median-of-arrays.ts
+++ src/hard/004-median-of-arrays.ts
@@ -2,7 +2,8 @@ export function findMedianSortedArrays(
   nums1: number[],
   nums2: number[]
 ): number {
-  const sortedOverall = [...nums1, ...nums2].sort((a, b) => (a < b ? -1 : 1));
+  //  const sortedOverall = [...nums1, ...nums2].sort((a, b) => (a < b ? -1 : 1));
+  const sortedOverall = buildSorted(nums1, nums2);
   if (sortedOverall.length % 2 !== 0)
     return sortedOverall[Math.floor(sortedOverall.length / 2)];
   return (
@@ -10,4 +11,32 @@ export function findMedianSortedArrays(
       sortedOverall[sortedOverall.length / 2]) /
     2
   );
+}
+
+function buildSorted(nums1: number[], nums2: number[]): number[] {
+  const result: number[] = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < nums1.length || j < nums2.length) {
+    if (i >= nums1.length) {
+      result.push(nums2[j]);
+      j++;
+      continue;
+    }
+    if (j >= nums2.length) {
+      result.push(nums1[i]);
+      i++;
+      continue;
+    }
+    if (nums1[i] <= nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+    } else {
+      result.push(nums2[j]);
+      j++;
+    }
+  }
+
+  return result;
 }
